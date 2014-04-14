f8885fe15d9d073577010214275311639578d831
Dave Powers <djpowers89@gmail.com>
2014-04-13 20:23:30 -0400

    Randomize list ID on creation for semi-privacy

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,9 +1,18 @@
 class List < ActiveRecord::Base
 
+  before_create :randomize_id
+
   validates_presence_of :title, :subject_a, :subject_b, :adjective, :user
 
   belongs_to :user
   has_many :reasons,
     dependent: :destroy
+
+
+  def randomize_id
+    begin
+      self.id = SecureRandom.random_number(1_000_000)
+    end while List.where(id: self.id).exists?
+  end
 
 end
