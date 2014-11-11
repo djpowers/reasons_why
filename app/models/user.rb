b63a465bc5e4d398f114e739ab94fd5053f497a2
Dave Powers <djpowers89@gmail.com>
2014-11-10 21:00:49 -0500

    Update syntax to fix omniauth facebook. Update test coverage.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
     dependent: :destroy
 
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.provider = auth.provider
       user.uid = auth.uid
